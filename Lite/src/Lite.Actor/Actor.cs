07e360be72a468d48d9d1f16f490e404c4843e8b
Administrator <Administrator@ADMINISTRATOR>
2016-05-27 17:10:33 +0800

    add lite actor project

---
+++ Lite/src/Lite.Actor/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lite.Actor
+{
+    public abstract class Actor
+    {
+        public string Name { get; set; }
+    }
+}
